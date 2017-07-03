d020b01b0eff4e1a036129d71104b17d5d5459df
j.mortier <j.mortier@qwant.com>
2017-07-03 16:04:21 +0200

    Start group games

---
+++ src/modules/play/public/javascript/play.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Play (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Play.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Play.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Play;
+}();
